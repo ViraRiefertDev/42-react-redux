1c3ee1ef8e4f61b10e9986d6e58c6b081bf2d14b
ViraRiefertDev <vira.riefert.dev@gmail.com>
2025-01-10 07:22:38 +0100

    conusult08

---
+++ src/consultation/Consultation08/Consultation08.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { ConsultationWrapper } from "./styles"
+import Search from "../../components/Search/Search"
+
+function Consultation08() {
+  return (
+    <ConsultationWrapper>
+      <Search />
+    </ConsultationWrapper>
+  )
+}
+
+export default Consultation08
---
+++ src/store/redux/counter/counterSlice.ts
@@ -0,0 +1,33 @@
+//1. Имопртируем функцию, с помощью которой создаем slice
+import { createAppSlice } from "../../createAppSlice"
+
+import { CounterStateSlice } from "./types"
+//4.1 Создаем объект с первоначальным состоянием, который мы потом передаем в initialState
+const counterInitialState: CounterStateSlice = {
+  count: 0,
+}
+
+//2. Создаем slice для counter
+export const counterSlice = createAppSlice({
+  //3. Задаем имя, под которым будет храниться объект со значением counterA (state) в глобальном state
+  name: "COUNTER",
+  //4. Задаем объект, в котором будет храниться начальное состояние
+  initialState: counterInitialState,
+  //5. Создаем объект, внутри которого будут храниться reducers(функции, которые отвечают за именение состояния)
+  reducers: create => ({
+    plus: create.reducer((state: CounterStateSlice) => {
+      state.count = state.count + 1
+    }),
+    minus: create.reducer((state: CounterStateSlice) => {
+      state.count = state.count - 1
+    }),
+  }),
+  //6. Создаем селекторы, которые позволяют забрать данные из стейта в компонент
+  selectors: {
+    count: (state: CounterStateSlice) => state.count,
+  },
+});
+
+//7. Экспорт экшенов и селекторов для возможности их использования в компонентах
+export const counterSliceActions = counterSlice.actions
+export const counterSliceSelectors = counterSlice.selectors
